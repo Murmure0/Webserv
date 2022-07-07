e797a4011a64ae1ed9c1411bf39b06629bca7b3a
Maelle Berthet <berthet.maelle@hotmail.fr>
2022-07-07 15:51:16 +0200

    Preparation projet

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "../includes/header.hpp"
+
+int main(int argc, char** argv)
+{
+	if (argc == 2)
+	{
+		/*recup le fichier de config : argv[2]
+			parsing du fichier dans differentes classes : 
+				verifier que le fichier de config est ok : pas de fichier vide, des {} fermées ...
+				une classe qui reprends l'ensemble des infos
+				une classe/port écouté avec ses specificités
+			but : selon la requete client, trouver le bon port et utiliser la config associée
+		*/
+
+		/*lancer le serveur en boucle infinie :
+			creer un/plusieurs socket : 1 par ports a ecouter (socket)
+			lier un socket avec un port a ecouter (bind)
+			ecouter en mode non bloquant les requetes clients (listen /accept)
+				parser les requetes clients
+					identifier sur quel port on ecoute et a quel partie du serveur transmettre la requete
+					identifier la requete du client et si c'est possible d'y repondre 
+						en fonction des parametres de la partie consernée
+				
+				envoyer une reponse au client 
+
+			fini ? Fermer les sockets
+		*/
+
+		/* creer les types de reponses avec leur messages,
+			 a transmettre en fonction de la requete : 1xx,2xx,3xx,4xx ou 5xx
+		*/
+
+
+	}
+	return 0;
+}
